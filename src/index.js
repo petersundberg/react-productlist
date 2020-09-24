6c7ffe83cb5a992ed7bac8d46b3b4f52243a3fa4
hassehulabeck <hansbudandersson@gmail.com>
2020-09-24 15:31:00 +0200

    Added css and sorting

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 
 class Productlist extends React.Component {
     constructor() {
@@ -26,8 +27,14 @@ class Productlist extends React.Component {
         };
     }
 
-    handleSubmit(event) {
-        event.preventDefault();
+    sortProducts() {
+        let tempProducts = this.state.products;
+        tempProducts.sort((a, b) => {
+            return b.price - a.price;
+        });
+        this.setState({
+            products: tempProducts,
+        });
     }
 
     render() {
@@ -35,9 +42,12 @@ class Productlist extends React.Component {
             <div>
                 <ul>
                     {this.state.products.map((product) => (
-                        <Productinfo product={product} />
+                        <Productinfo key={product.id} product={product} />
                     ))}
                 </ul>
+                <button onClick={() => this.sortProducts()}>
+                    Sortera efter pris (fallande)
+                </button>
             </div>
         );
     }
@@ -45,9 +55,9 @@ class Productlist extends React.Component {
 
 function Productinfo(props) {
     return (
-        <article>
+        <article className="produkt">
             <h3>{props.product.name}</h3>
-            <p>{props.product.price}</p>
+            <p>{props.product.price} inkl moms</p>
         </article>
     );
 }
