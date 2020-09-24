a42d4d7d2f0f7c8b8c90304fc65b62c57b8ad871
hassehulabeck <hansbudandersson@gmail.com>
2020-09-24 14:01:33 +0200

    First try with productinfo

--- src/index.js
+++ src/index.js
@@ -1,51 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Citylister extends React.Component {
+class Productlist extends React.Component {
     constructor() {
         super();
         this.state = {
-            sweCities: ["Stockholm", "Göteborg", "Lund", "Växjö", "Härnösand"],
-            money: 100,
+            products: [
+                {
+                    id: 2,
+                    name: "Hammare B14",
+                    price: 129,
+                },
+                {
+                    id: 4,
+                    name: "Fiskespö Celeste",
+                    price: 799,
+                    weight: 230,
+                },
+                {
+                    id: 3,
+                    name: "Frontlastare Volvo B16",
+                    price: 35000,
+                },
+            ],
         };
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        /* Att direkt modifiera en array går inte i React, 
-        därför kan man lösa det så här */
-
-        // Create a new array based on current state:
-        let sweCities = [...this.state.sweCities];
-
-        // Add item to it
-        sweCities.push(event.target.newcity.value);
-
-        // Set state
-        this.setState({ sweCities: sweCities });
     }
 
     render() {
         return (
             <div>
                 <ul>
-                    {this.state.sweCities.map((city) => (
-                        <li>{city}</li>
+                    {this.state.products.map((product) => (
+                        <Productinfo product={product} />
                     ))}
                 </ul>
-                <ShowMoney money={this.state.money} />
-                <form onSubmit={this.handleSubmit}>
-                    <input name="newcity" type="text" />
-                    <input type="submit" value="Lägg till stad" />
-                </form>
             </div>
         );
     }
 }
 
-function ShowMoney(props) {
-    return <p>{props.money - 5}</p>;
+function Productinfo(props) {
+    return (
+        <article>
+            <h3>{props.product.name}</h3>
+            <p>{props.product.price}</p>
+        </article>
+    );
 }
 
-ReactDOM.render(<Citylister />, document.getElementById("root"));
+ReactDOM.render(<Productlist />, document.getElementById("root"));
